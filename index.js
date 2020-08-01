23200bafe22a4bc90da0849803646900c263fcb1
Mathas <sea199676@hotmail.co.th>
2020-08-01 17:50:46 +0700

    add temperature func.

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const { blink } = require('./blink');
+const { thermometer } = require('./temperature');
 
 try {
-    blink(); 
+    thermometer(); 
 } catch (error) {
     console.log(error);
 }
---
+++ temperature.js
@@ -0,0 +1,23 @@
+const { board } = require('./connection')
+const { Thermometer } = require("johnny-five");
+
+const thermometer = () => {
+    board.on("ready", () => {
+        const thermometer = new Thermometer({
+            controller: "DS18B20",
+            pin: "3"
+        });
+
+        thermometer.on("change", () => {
+            const { celsius, fahrenheit, kelvin } = thermometer;
+            console.log("Thermometer");
+            console.log("  celsius      : ", celsius);
+            console.log("  fahrenheit   : ", fahrenheit);
+            console.log("  kelvin       : ", kelvin);
+            console.log("--------------------------------------");
+        });
+    });
+}
+
+
+module.exports = { thermometer }
